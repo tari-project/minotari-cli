d012bf7ddb172117a773dc790646d4152d3af8dc
stringhandler <stringhandler@protonmail.com>
2025-10-14 14:42:59 +0200

    fetch working

--- src/tapplets/fetch.rs
+++ src/tapplets/fetch.rs
@@ -5,10 +5,7 @@ use tari_tapplet_lib::registry::TappletRegistry;
 pub async fn fetch(cache_directory: PathBuf) -> Result<(), anyhow::Error> {
     println!("Fetching tapplets from all registries...");
 
-    let default_registries = vec![(
-        "Tari Official",
-        "https://github.com/tari-project/tapplet-registry/raw/main/registry.json",
-    )];
+    let default_registries = vec![("Tari Official", "https://github.com/tari-project/tapplet-registry")];
 
     for (name, url) in default_registries {
         println!("Fetching from registry: {} ({})", name, url);
@@ -16,7 +13,7 @@ pub async fn fetch(cache_directory: PathBuf) -> Result<(), anyhow::Error> {
         let mut registry = TappletRegistry::new(name, url, cache_directory.clone());
         registry.fetch().await?;
 
-        println!("Registy is not at revision:{:?}", registry.revision());
+        println!("Registy is now at revision:{:?}", registry.revision());
         // let tapplets = registry.get_tapplets().await?;
         // println!("Found {} tapplets in registry '{}':", tapplets.len(), name);
         // for tapplet in tapplets {
