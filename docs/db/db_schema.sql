4b36c6b60ec9aa1bf4089126785c5534c8914261
Martins Erts <fp@tari.com>
2025-11-04 14:42:12 +0200

    Update db schema

--- docs/db/db_schema.sql
+++ docs/db/db_schema.sql
@@ -79,7 +79,6 @@ CREATE TABLE inputs (
 CREATE INDEX idx_scanned_tip_blocks_account_height ON scanned_tip_blocks(account_id, height DESC);
 CREATE UNIQUE INDEX idx_outputs_output_hash ON outputs(output_hash);
 CREATE INDEX idx_outputs_account_mined_height ON outputs(account_id, mined_in_block_height);
-CREATE INDEX idx_balance_changes_account_id ON balance_changes(account_id);
 CREATE INDEX idx_inputs_output_id ON inputs(output_id);
 CREATE UNIQUE INDEX idx_inputs_output_id_unique ON inputs(output_id);
 CREATE UNIQUE INDEX idx_scanned_tip_blocks_account_height_hash ON scanned_tip_blocks(account_id, height, hash);
@@ -96,3 +95,5 @@ CREATE TABLE pending_transactions (
     UNIQUE (account_id, idempotency_key)
 );
 CREATE INDEX idx_pending_transactions_status_expires_at ON pending_transactions(status, expires_at);
+CREATE INDEX idx_balance_changes_account_height ON balance_changes(account_id, effective_height);
+CREATE INDEX idx_inputs_account_mined_height ON inputs(account_id, mined_in_block_height);
